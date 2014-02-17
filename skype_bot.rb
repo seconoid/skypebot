eb18bcc7fad09b1aef3c5baec67eb89677687e7b
seconoid <seconoid@gmail.com>
2014-02-17 20:09:32 +0900

    use a regular expression and add a drink.

--- skype_bot.rb
+++ skype_bot.rb
@@ -16,24 +16,39 @@ loop do
       next unless Time.now.to_i - last_time < hearing_threshold
 
       # chat
-      chat.post "pong" if m.body.include? "ping"
-      chat.post "はい。" if m.body.include? "ありすさん"
-      chat.post "そうでもないです。" if m.body.include? "かわいいですね"
-      chat.post "蹴っ飛ばしますよ。" if m.body.include? "誰にでも股開くんですね"
-      chat.post "次から気をつけてください。" if m.body.include? "ごめんなさい"
-      if m.body.include? "喉が渇きました"
-        chat.post "紅茶をお淹れしますね。" if last_time % 2 == 0
-        chat.post "コーヒーをお淹れしますね。" if last_time % 2 == 1
+      drink = ["紅茶", "コーヒー", "アイスティー"].sample
+
+      if m.body =~ /ping/
+        chat.post "pong" 
+      elsif m.body =~ /ありすさん/
+        chat.post "はい。"
+      elsif m.body =~ /かわいいですね/
+        chat.post "そうでもないです。"
+      elsif m.body =~ /誰にでも股開くんですね/
+        chat.post "蹴っ飛ばしますよ。" 
+      elsif m.body =~ /ごめんなさい/
+        chat.post "気をつけてください。"
+      elsif m.body =~ /喉が渇きました/
+        chat.post "#{drink}をお淹れしますね。"
       end
      
       # time
-      chat.post Time.now if m.body.include? "alice ruby puts Time.now"
+      if m.body =~ /^alice?\s?plz?\s?current?\s?time$/
+        chat.post Time.now
+      end 
 
       # id
-      chat.post m.id if m.body.include? "alice ruby puts id"
+      if m.body =~ /^alice?\s?plz?\s?id$/
+        chat.post m.id
+      end
 
       #dice
-      chat.post Random.rand(5) + 1 if m.body.include? "alice plz dice"
+      if m.body =~ /^alice?\s?plz?\s?dice$/
+        chat.post Random.rand(6) + 1
+      end
+
+      #title
+
 
       last_id = m.id
     end
